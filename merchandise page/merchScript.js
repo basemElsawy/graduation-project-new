50faf6e629b8c2d66fd94065a3ad17a285133b97
Basem Elsawy <besoelpop2@gmail.com>
2022-09-14 23:32:48 +0200

    merchendise checkout edit

--- merchandise page/merchScript.js
+++ merchandise page/merchScript.js
@@ -1,11 +1,37 @@
+'use strict';
 let header = document.querySelector('.header');
 let logo = document.querySelector('.logo');
 let navbar = document.querySelector('.navbar');
 let logoImg = document.querySelector('.muscle-img');
+let shoppingCart = document.querySelector('.shopping-cart');
+let cartBtn = document.querySelector('.cart-btn');
+let merchCounter = document.getElementById('merch-count');
+let prices = document.querySelectorAll('.price-tags');
+let products = document.querySelectorAll('.container');
+let addToCart = document.querySelectorAll('.add-cart');
+let sideCheckoutMenu = document.querySelector('.side-bar');
+let closeBtn = document.querySelector('.close-sidebar');
+//-------------------========== VARIABLES =========----------------
 
-// --------------============= VARIABLES ================------------
+prices[0].innerText = Number(prices[0].innerText);
+merchCounter.innerText = Number(merchCounter.innerText);
+
 let counter = 1;
-// -----------========= FUNCTIONS & EVENT LISTENERS =======--------
+//------------============== MERCH FUNCTIONALITY =========-------------
+addToCart.forEach((btn) => {
+  btn.addEventListener('click', () => {
+    merchCounter.innerText++;
+  });
+});
+cartBtn.addEventListener('click', () => {
+  sideCheckoutMenu.style.width = '30%';
+});
+
+closeBtn.addEventListener('click', () => {
+  sideCheckoutMenu.style.width = '0%';
+});
+
+//---------================= SCROLLING FUNCTIONALITY=======-------------------
 document.addEventListener('DOMContentLoaded', function (event) {
   window.onscroll = function () {
     scrollFunction();
@@ -26,20 +52,34 @@ let scrollFunction = () => {
     document.body.scrollTop > 600 ||
     document.documentElement.scrollTop > 600
   ) {
+    // shoppingCart.style.visibility = 'visible';
     header.style.borderRadius = '0px 0px 10px 10px';
     header.style.marginRight = '10px';
     header.style.marginLeft = '10px';
   } else {
+    // shoppingCart.style.visibility = 'hidden';
     header.style.borderRadius = '0px 0px 0px 0px';
     header.style.marginRight = '0px';
     header.style.marginLeft = '0px';
   }
-};
-
-setInterval(() => {
-  document.getElementById('radio' + counter).checked = true;
-  counter++;
-  if (counter > 4) {
-    counter = 1;
+  if (
+    document.body.scrollTop > 300 ||
+    document.documentElement.scrollTop > 300
+  ) {
+    cartBtn.style.visibility = 'visible';
+    shoppingCart.style.visibility = 'visible';
+  } else {
+    cartBtn.style.visibility = 'hidden';
+    shoppingCart.style.visibility = 'hidden';
   }
-}, 2000);
+  if (
+    document.body.scrollTop > 305 ||
+    document.documentElement.scrollTop > 305
+  ) {
+    shoppingCart.style.transform = 'scale(1)';
+    shoppingCart.style.borderRadius = '10px 0px 0px 10px';
+  } else {
+    shoppingCart.style.transform = 'scale(0.05)';
+    shoppingCart.style.borderRadius = '10px';
+  }
+};
