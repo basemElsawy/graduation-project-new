78267ba11a5d93857b41032f1a8e22dba9e4713b
Basem Elsawy <besoelpop2@gmail.com>
2022-09-24 17:49:46 +0200

    finished styling for assesment page

---
+++ assessment page/assessingBehavior.js
@@ -0,0 +1,9 @@
+'use strict';
+
+let firstName = document.getElementById('Firstname');
+let lastName = document.getElementById('Lastname');
+let age = document.getElementById('Age');
+let height = document.getElementById('Height');
+let weight = document.getElementById('Weight');
+let submitBtn = document.getElementsByClassName('submit');
+let resultsDiv = document.getElementsByClassName('results');
