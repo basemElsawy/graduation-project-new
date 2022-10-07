94882f1fecfa840e38db9e327ccf628bc0c8b9b7
Basem Elsawy <besoelpop2@gmail.com>
2022-10-08 00:53:06 +0200

    started a new page workout area

---
+++ starting workout/workoutBehavior.js
@@ -0,0 +1,35 @@
+let header = document.querySelector('.header');
+let logo = document.querySelector('.logo');
+let navbar = document.querySelector('.navbar');
+let logoImg = document.querySelector('.muscle-img');
+
+// -----------========= FUNCTIONS & EVENT LISTENERS =======--------
+document.addEventListener('DOMContentLoaded', function (event) {
+  window.onscroll = function () {
+    scrollFunction();
+  };
+});
+
+let scrollFunction = () => {
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    header.style.boxShadow = '0px 2px 40px 2px black';
+    logo.style.transform = 'scale(1)';
+    navbar.style.transform = 'scale(1)';
+  } else {
+    navbar.style.transform = 'scale(1.2)';
+    logo.style.transform = 'scale(1.2)';
+    header.style.boxShadow = 'none';
+  }
+  if (
+    document.body.scrollTop > 750 ||
+    document.documentElement.scrollTop > 750
+  ) {
+    header.style.borderRadius = '0px 0px 10px 10px';
+    header.style.marginRight = '10px';
+    header.style.marginLeft = '10px';
+  } else {
+    header.style.borderRadius = '0px 0px 0px 0px';
+    header.style.marginRight = '0px';
+    header.style.marginLeft = '0px';
+  }
+};
