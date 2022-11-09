26794c349e7d1c4a7740dd1d65c1f2de5930b2b4
Basem Elsawy <besoelpop2@gmail.com>
2022-11-10 01:21:15 +0200

    local guide styling

--- local guide/guideBehavior.js
+++ local guide/guideBehavior.js
@@ -1,6 +1,26 @@
 let lines = document.querySelectorAll('.side-line');
 let circles = document.querySelectorAll('.circle');
 let strengthLibraries = document.querySelectorAll('.strength-libraries');
+let frstMainHeader = document.querySelector('.first-main-header');
+let headingSection = document.querySelector('.heading-section');
+let frstSideHeader = document.querySelector('.first-side-header');
+
+let secondContentAnimation = document.querySelectorAll('.second-content');
+
+const secondContentObserver = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('content-to-right');
+    } else {
+      entry.target.classList.remove('content-to-right');
+    }
+  });
+});
+
+secondContentAnimation.forEach((el) => {
+  secondContentObserver.observe(el);
+});
+
 const observer = new IntersectionObserver((entries) => {
   entries.forEach((entry) => {
     if (entry.isIntersecting) {
@@ -42,3 +62,24 @@ const librariesOnscroll = new IntersectionObserver((entries) => {
 strengthLibraries.forEach((el) => {
   librariesOnscroll.observe(el);
 });
+
+window.onscroll = function () {
+  if (
+    document.documentElement.scrollTop >= 50 &&
+    document.documentElement.scrollTop <= 900
+  ) {
+    frstMainHeader.classList.add('text-to-left');
+  } else {
+    console.log('reached');
+    frstMainHeader.classList.remove('text-to-left');
+  }
+  if (
+    document.documentElement.scrollTop >= 100 &&
+    document.documentElement.scrollTop <= 1200
+  ) {
+    frstSideHeader.classList.add('text-to-right');
+  } else {
+    console.log('reached');
+    frstSideHeader.classList.remove('text-to-right');
+  }
+};
